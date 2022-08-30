389a15dc2fe79811b05f38ff13f9701305c46de7
laurenp305 <lamor1800@gmail.com>
2022-08-30 17:14:19 -0400

    created roles table

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,21 @@ DROP DATABASE IF EXISTS team_db;
 CREATE DATABASE team_db;
 USE team_db;
 
--- creates department column --
+-- creates department table --
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(25) NOT NULL,
     PRIMARY KEY (id)
 );
 
+-- creates roles table with foregin key --
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  job_title VARCHAR(40) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(deptartment_id) REFERENCES departments(id)
+);
 
 
